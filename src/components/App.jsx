6ddb98c9c20d2dbe436406b04c90e125ff081f12
kudo07 <134780035+kudo07@users.noreply.github.com>
2024-01-20 02:36:45 +0530

    asd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ function App() {
           <Route path="/movie/:id" element={<MovieInfo />}></Route>
           <Route path="/actors/:id" element={<Actors />}></Route>
           <Route path="/" element={<Movies />}></Route>
-          <Route path="/approved" element={<Movies />}></Route>
+          {/* <Route path="/approved" element={<Movies />}></Route> */}
           <Route path="/profile/:id" element={<Profile />}></Route>
         </Routes>
       </main>
