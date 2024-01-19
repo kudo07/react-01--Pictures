834239c012d09bcf67fa8251006c34902ca88114
kudo07 <134780035+kudo07@users.noreply.github.com>
2024-01-20 02:25:32 +0530

    final

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './components/App';
+import store from './src/app/store';
+import App from './src/components/App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
